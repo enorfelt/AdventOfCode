591fc706075ad56c474090054ab58045484c180b
Erik Norfelt <erik.norfelt@monitor.se>
2022-12-06 20:44:51 +0100

    Prep for day 7

---
+++ src/2022/Day7/AdventOfCode.Day7.Tests/Day7Tests.cs
@@ -0,0 +1,11 @@
+namespace AdventOfCode.Day7.Tests
+{
+	public class Day7Tests
+	{
+		[Fact]
+		public void Test1()
+		{
+
+		}
+	}
+}
---
+++ src/2022/Day7/AdventOfCode.Day7/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+var input = File.ReadAllLines("Input.txt");
+Console.WriteLine("Hello, World!");
